796b741d874c1a1916ed6ae7bc706deb1726cc2a
eae-mendoza <50602943+eae-mendoza@users.noreply.github.com>
2021-08-04 11:54:29 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
     ],
     packages=["reader"],
     include_package_data=True,
-    install_requires=["scipy","sklearn","matplotlib","pyrmle_hfuncs],
+    install_requires=["scipy","sklearn","matplotlib","pyrmle_hfuncs"],
 )
