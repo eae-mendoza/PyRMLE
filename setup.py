709f18d7d7069b2e25ee7e104b6a1d74c4dc10b7
eae-mendoza <50602943+eae-mendoza@users.noreply.github.com>
2021-08-04 11:58:25 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,23 +8,25 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
-setup(
-    name="pyrmle",
-    version="1.0",
-    description="Python package for implementing Regularized Maximum Likelihood for Random Coefficient Models",
-    long_description=README,
-    url="https://github.com/eae-mendoza/PyRMLE",
-    author="Emil Alfred Edgar N. Mendoza",
-    author_email="emil.edgar.mendoza@gmail.com",
-    license="BSD",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
-    ],
-    packages=["pyrmle","pyrmle_hfuncs"],
-    include_package_data=True,
-    install_requires=["scipy","sklearn","matplotlib"],
-)
+
+if __name__ == "__main__":
+    setup(
+        name="pyrmle",
+        version="1.0",
+        description="Python package for implementing Regularized Maximum Likelihood for Random Coefficient Models",
+        long_description=README,
+        url="https://github.com/eae-mendoza/PyRMLE",
+        author="Emil Alfred Edgar N. Mendoza",
+        author_email="emil.edgar.mendoza@gmail.com",
+        license="BSD",
+        classifiers=[
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9"
+        ],
+        packages=["pyrmle","pyrmle_hfuncs"],
+        include_package_data=True,
+        install_requires=["scipy","sklearn","matplotlib"],
+    )
